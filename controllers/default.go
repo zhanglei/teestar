a1b2999236d0cfc62deb78ea8d60e5741f02b6f0
Yang Luo <hsluoyz@qq.com>
2017-07-22 21:22:13 +0800

    Add DeleteUserRepo().

--- controllers/default.go
+++ controllers/default.go
@@ -28,13 +28,7 @@ func (c *MainController) GetUserAllRepos() {
 	c.ServeJSON()
 }
 
-func (c *MainController) GetUserRepos() {
-	user := c.GetString(":user")
-
-	//c.Data["Website"] = "beego.me"
-	//c.Data["Email"] = "astaxie@gmail.com"
-	//c.TplName = "index.tpl"
-
+func getUserRepos(user string) []string {
 	userRepos := UserRepos{User:user}
 	has, err := adapter.engine.Get(&userRepos)
 	if err != nil {
@@ -42,10 +36,52 @@ func (c *MainController) GetUserRepos() {
 	}
 
 	if !has {
-		c.Data["json"] = []string{}
-		c.ServeJSON()
+		return []string{}
+	} else {
+		return strings.Split(userRepos.Repos, ",")
+	}
+}
+
+func (c *MainController) GetUserRepos() {
+	user := c.GetString(":user")
+
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+
+	c.Data["json"] = getUserRepos(user)
+	c.ServeJSON()
+}
+
+func deleteUserRepo(user string, repo string) bool {
+	repos := getUserRepos(user)
+	var newRepos []string
+	for _, r := range repos {
+		if repo != r {
+			newRepos = append(newRepos, r)
+		}
 	}
 
-	c.Data["json"] = strings.Split(userRepos.Repos, ",")
+	userRepos := UserRepos{User:user, Repos:strings.Join(newRepos, ",")}
+	affected, err := adapter.engine.Update(&userRepos)
+	if err != nil {
+		panic(err)
+	}
+
+	return affected != 0
+}
+
+func (c *MainController) DeleteUserRepo() {
+	user := c.GetString(":user")
+	repo := c.GetString(":repo")
+	repo = strings.Replace(repo, ".", "/", -1)
+
+	affected := deleteUserRepo(user, repo)
+
+	if affected {
+		c.Data["json"] = "ok"
+	} else {
+		c.Data["json"] = "not affected"
+	}
 	c.ServeJSON()
 }
--- routers/router.go
+++ routers/router.go
@@ -8,4 +8,5 @@ import (
 func init() {
     beego.Router("/api/users/:user/all-repos", &controllers.MainController{},"get:GetUserAllRepos")
 	beego.Router("/api/users/:user/repos", &controllers.MainController{}, "get:GetUserRepos")
+	beego.Router("/api/users/:user/repos/delete/:repo", &controllers.MainController{}, "get:DeleteUserRepo")
 }
