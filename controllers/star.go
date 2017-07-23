9c3946eda9f58dd2a159862a6c6d23001a52b55a
Yang Luo <hsluoyz@qq.com>
2017-07-23 12:59:42 +0800

    Add UpdateUserStarringRepos().

--- controllers/star.go
+++ controllers/star.go
@@ -1,5 +1,7 @@
 package controllers
 
+import "github.com/hsluoyz/gitstar/api"
+
 func getUserStarringRepos(user string) []string {
 	var userRepos []UserStarringRepo
 	err := adapter.engine.Find(&userRepos, &UserStarringRepo{User: user})
@@ -19,5 +21,39 @@ func (c *MainController) GetUserStarringRepos() {
 	user := c.GetString(":user")
 
 	c.Data["json"] = getUserStarringRepos(user)
+	c.ServeJSON()
+}
+
+func updateUserStarringRepos(user string) bool {
+	repos := api.ListStarringRepos(user)
+
+	affected, err := adapter.engine.Delete(&UserStarringRepo{User: user})
+	if err != nil {
+		panic(err)
+	}
+
+	userRepos := []UserStarringRepo{}
+	for _, repo := range repos {
+		userRepos = append(userRepos, UserStarringRepo{User: user, Repo: repo})
+	}
+
+	affected, err = adapter.engine.Insert(&userRepos)
+	if err != nil {
+		panic(err)
+	}
+
+	return affected != 0
+}
+
+func (c *MainController) UpdateUserStarringRepos() {
+	user := c.GetString(":user")
+
+	affected := updateUserStarringRepos(user)
+
+	if affected {
+		c.Data["json"] = "ok"
+	} else {
+		c.Data["json"] = "not affected"
+	}
 	c.ServeJSON()
 }
--- routers/router.go
+++ routers/router.go
@@ -10,7 +10,9 @@ func init() {
 	beego.Router("/api/users/:user/repos", &controllers.MainController{}, "get:GetUserRepos")
 	beego.Router("/api/users/:user/repos/add/:repo", &controllers.MainController{}, "get:AddUserRepo")
 	beego.Router("/api/users/:user/repos/delete/:repo", &controllers.MainController{}, "get:DeleteUserRepo")
+
 	beego.Router("/api/users/:user/starring-repos", &controllers.MainController{}, "get:GetUserStarringRepos")
+	beego.Router("/api/users/:user/starring-repos/update", &controllers.MainController{}, "get:UpdateUserStarringRepos")
 
 	beego.Router("/api/users/:user/targets/:target", &controllers.MainController{}, "get:GetUserTarget")
 	beego.Router("/api/users/:user/targets/:target/status", &controllers.MainController{}, "get:GetUserTargetStatus")
