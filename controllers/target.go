48af7c7b7a0f5807fac90a186777b168a8dc8807
Yang Luo <hsluoyz@qq.com>
2017-07-23 19:15:32 +0800

    Add GetUserTargetPool().

--- controllers/target.go
+++ controllers/target.go
@@ -17,6 +17,38 @@ func getIntersect(a []string, b []string) []string {
 	return res.Interface().([]string)
 }
 
+func getSubtract(a []string, b []string) []string {
+	res := []string{}
+	for _, ia := range a {
+		found := false
+		for _, ib := range b {
+			if ia == ib {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			res = append(res, ia)
+		}
+	}
+
+	return res
+}
+
+func Get(a []string, b []string) []string {
+	if len(a) == 0 || len(b) == 0 {
+		return []string{}
+	}
+
+	res, ok := Intersect(a, b)
+	if !ok {
+		panic(errors.New("cannot find intersect"))
+	}
+
+	return res.Interface().([]string)
+}
+
 func (c *MainController) GetUserTarget() {
 	user := c.GetString(":user")
 	target := c.GetString(":target")
@@ -48,5 +80,17 @@ func (c *MainController) GetUserTargetStatus() {
 	score := len(starredRepos) - len(starringRepos)
 
 	c.Data["json"] = UserTargetStatus{StarringRepos: starringRepos, StarredRepos: starredRepos, Score: score}
+	c.ServeJSON()
+}
+
+func (c *MainController) GetUserTargetPool() {
+	user := c.GetString(":user")
+	target := c.GetString(":target")
+
+	targetRepos := getUserRepos(target)
+	userStarringRepos := getUserStarringRepos(user)
+	poolRepos := getSubtract(targetRepos, userStarringRepos)
+
+	c.Data["json"] = poolRepos
 	c.ServeJSON()
 }
--- routers/router.go
+++ routers/router.go
@@ -18,4 +18,5 @@ func init() {
 
 	beego.Router("/api/users/:user/targets/:target", &controllers.MainController{}, "get:GetUserTarget")
 	beego.Router("/api/users/:user/targets/:target/status", &controllers.MainController{}, "get:GetUserTargetStatus")
+	beego.Router("/api/users/:user/targets/:target/pool", &controllers.MainController{}, "get:GetUserTargetPool")
 }
