40a92a5b6e071bf626975ccc34c74ae130d6a4ba
Yang Luo <hsluoyz@qq.com>
2017-07-24 18:41:22 +0800

    Use file as session.

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	// "github.com/astaxie/beego"
-	_ "github.com/hsluoyz/gitstar/routers"
-	//"github.com/google/go-github/github"
-	//"context"
-	//"fmt"
 	"github.com/astaxie/beego"
+	_ "github.com/hsluoyz/gitstar/routers"
 )
 
 func main() {
+	beego.BConfig.WebConfig.Session.SessionProvider="file"
+	beego.BConfig.WebConfig.Session.SessionProviderConfig = "./tmp"
+
 	beego.Run()
 }
 
