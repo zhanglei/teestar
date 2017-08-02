126361c4691322220cd02b646b1391dae636c6a8
Yang Luo <hsluoyz@qq.com>
2017-08-02 11:22:27 +0800

    Add IP module.

---
+++ api/ip.go
@@ -0,0 +1,31 @@
+package api
+
+func init() {
+	err := Init("data/17monipdb.dat")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func IsMainland(ip string) bool {
+	info, err := Find(ip)
+	if err != nil {
+		panic(err)
+	}
+
+	return info.Country == "中国"
+}
+
+func GetDescFromIP(ip string) string {
+	info, err := Find(ip)
+	if err != nil {
+		panic(err)
+	}
+
+	res := info.Country + ", " + info.Region + ", " + info.City
+	if info.Isp != Null {
+		res += ", " + info.Isp
+	}
+
+	return res
+}
