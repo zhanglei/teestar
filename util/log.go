ce464e102418542e64a2583d076477d034819e6d
Yang Luo <hsluoyz@qq.com>
2017-08-02 11:23:04 +0800

    Parse IP to country in log.

--- util/log.go
+++ util/log.go
@@ -1,10 +1,22 @@
 package util
 
 import (
+	"fmt"
+
 	"github.com/astaxie/beego/context"
 	"github.com/astaxie/beego/logs"
+	"github.com/hsluoyz/gitstar/api"
 )
 
 func LogInfo(ctx *context.Context, f string, v ...interface{}) {
-	logs.Info("(" + ctx.Request.RemoteAddr + ") " + f, v...)
+	var ipString string
+	clientIP := ctx.Request.Header.Get("x-forwarded-for")
+	if clientIP != "" {
+		desc := api.GetDescFromIP(clientIP)
+		ipString = fmt.Sprintf("(%s: %s) ", clientIP, desc)
+	} else {
+		ipString = "() "
+	}
+
+	logs.Info(ipString + f, v...)
 }
