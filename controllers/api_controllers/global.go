b69aff1ddcaf97d77b0df55d7bc9babab94b52ac
Yang Luo <hsluoyz@qq.com>
2017-08-04 11:45:38 +0800

    Add docs to Global API.

--- controllers/api_controllers/global.go
+++ controllers/api_controllers/global.go
@@ -5,26 +5,43 @@ import (
 	"github.com/hsluoyz/gitstar/api"
 )
 
+// Global API
 type GlobalController struct {
 	beego.Controller
 }
 
+// @Title UpdateStarringRepos
+// @Description update all the repos starred by each user into GitStar cache
+// @Success 200 {object} controllers.api_controller.Response The response object
+// @router /starring-repos/update [get]
 func (c *GlobalController) UpdateStarringRepos() {
+	var resp Response
+
 	affected := api.UpdateStarringRepos()
 
 	if affected {
-		c.Data["json"] = "ok"
+		resp = Response{Code: 200, Msg: "ok", Data: ""}
 	} else {
-		c.Data["json"] = "not affected"
+		resp = Response{Code: 200, Msg: "not affected", Data: ""}
 	}
+
+	c.Data["json"] = resp
 	c.ServeJSON()
 }
 
+// @Title GetRecommend
+// @Description Get the recommend repos for each user
+// @Success 200 {object} []api.Entry2 The list of Entry2 objects
+// @router /recommend [get]
 func (c *GlobalController) GetRecommend() {
 	c.Data["json"] = api.GetRecommend()
 	c.ServeJSON()
 }
 
+// @Title GetOwe
+// @Description Get the details that each user owes another user stars
+// @Success 200 {object} []*api.UserTargetStatus The list of status objects
+// @router /owe [get]
 func (c *GlobalController) GetOwe() {
 	c.Data["json"] = api.GetOwe()
 	c.ServeJSON()
