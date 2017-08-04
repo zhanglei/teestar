2838d81a072134d3992c8b1262208774fd32c307
Yang Luo <hsluoyz@qq.com>
2017-08-04 10:28:24 +0800

    Add docs to Users Repo API.

--- controllers/api_controllers/users_repos.go
+++ controllers/api_controllers/users_repos.go
@@ -17,6 +17,11 @@ func getAllUserAndOrganRepos(user string) []string {
 	return repos
 }
 
+// @Title GetUserAllRepos
+// @Description Get all the repos the user has in GitHub, this API is actually not used yet
+// @Param   user     path    string  true        "The username"
+// @Success 200 {[]string}
+// @router /:user/repos/all [get]
 func (c *UsersController) GetUserAllRepos() {
 	user := c.GetString(":user")
 
@@ -25,6 +30,11 @@ func (c *UsersController) GetUserAllRepos() {
 	c.ServeJSON()
 }
 
+// @Title GetUserRepos
+// @Description Get all the repos the user added in GitStar
+// @Param   user     path    string  true        "The username"
+// @Success 200 {[]string}
+// @router /:user/repos [get]
 func (c *UsersController) GetUserRepos() {
 	user := c.GetString(":user")
 
@@ -32,7 +42,14 @@ func (c *UsersController) GetUserRepos() {
 	c.ServeJSON()
 }
 
+// @Title AddUserRepo
+// @Description Add a repo for the user
+// @Param   user     path    string  true        "The username"
+// @Param   repo     path    string  true        "The repository name, like user_name.repo_name"
+// @Success 200 {object} controllers.api_controller.Response The response object
+// @router /:user/repos/add/:repo [get]
 func (c *UsersController) AddUserRepo() {
+	var resp Response
 	user := c.GetString(":user")
 	repo := c.GetString(":repo")
 	repo = strings.Replace(repo, ".", "/", -1)
@@ -40,14 +57,23 @@ func (c *UsersController) AddUserRepo() {
 	affected := api.AddUserRepo(user, repo)
 
 	if affected {
-		c.Data["json"] = "ok"
+		resp = Response{Code: 200, Msg: "ok", Data: ""}
 	} else {
-		c.Data["json"] = "not affected"
+		resp = Response{Code: 200, Msg: "not affected", Data: ""}
 	}
+
+	c.Data["json"] = resp
 	c.ServeJSON()
 }
 
+// @Title DeleteUserRepo
+// @Description Delete a repo for the user
+// @Param   user     path    string  true        "The username"
+// @Param   repo     path    string  true        "The repository name, like user_name.repo_name"
+// @Success 200 {object} controllers.api_controller.Response The response object
+// @router /:user/repos/delete/:repo [get]
 func (c *UsersController) DeleteUserRepo() {
+	var resp Response
 	user := c.GetString(":user")
 	repo := c.GetString(":repo")
 	repo = strings.Replace(repo, ".", "/", -1)
@@ -55,9 +81,11 @@ func (c *UsersController) DeleteUserRepo() {
 	affected := api.DeleteUserRepo(user, repo)
 
 	if affected {
-		c.Data["json"] = "ok"
+		resp = Response{Code: 200, Msg: "ok", Data: ""}
 	} else {
-		c.Data["json"] = "not affected"
+		resp = Response{Code: 200, Msg: "not affected", Data: ""}
 	}
+
+	c.Data["json"] = resp
 	c.ServeJSON()
 }
