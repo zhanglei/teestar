0d728ad64d3e58df095f30f6fdc6026da00272e7
Yang Luo <hsluoyz@qq.com>
2017-08-04 22:50:59 +0800

    Add /api/users/:user/extended

--- controllers/api_controllers/users.go
+++ controllers/api_controllers/users.go
@@ -23,13 +23,26 @@ func (c *UsersController) GetUsers() {
 // @Title GetUser
 // @Description Get a user
 // @Param   user     path    string  true        "The username"
-// @Success 200 {object} api.User The user object
+// @Success 200 {object} api.User The User object
 // @router /:user [get]
 func (c *UsersController) GetUser() {
 	user := c.GetString(":user")
 
 	objUser := api.GetUser(user)
 	objUser.Password = ""
+	c.Data["json"] = objUser
+	c.ServeJSON()
+}
+
+// @Title GetExtendedUser
+// @Description Get a user with extended information
+// @Param   user     path    string  true        "The username"
+// @Success 200 {object} api.ExtendedUser The ExtendedUser object
+// @router /:user/extended [get]
+func (c *UsersController) GetExtendedUser() {
+	user := c.GetString(":user")
+
+	objUser := api.GetExtendedUser(user)
 	c.Data["json"] = objUser
 	c.ServeJSON()
 }
