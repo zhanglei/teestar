2ea872e51d27cb6ea6bd4fd42f55014df671f3e8
Yang Luo <hsluoyz@qq.com>
2017-08-04 11:18:45 +0800

    Add docs to Users Hitter API.

--- controllers/api_controllers/users_hitter.go
+++ controllers/api_controllers/users_hitter.go
@@ -2,6 +2,11 @@ package api_controllers
 
 import "github.com/hsluoyz/gitstar/api"
 
+// @Title GetUserHitter
+// @Description Get the hitter for a user
+// @Param   user     path    string  true        "The username"
+// @Success 200 {string}
+// @router /:user/hitter [get]
 func (c *UsersController) GetUserHitter() {
 	user := c.GetString(":user")
 
@@ -11,16 +16,25 @@ func (c *UsersController) GetUserHitter() {
 	c.ServeJSON()
 }
 
+// @Title UpdateUserHitter
+// @Description Get the hitter for a user
+// @Param   user     path    string  true        "The username"
+// @Param   hitter     path    string  true        "The GitHub account the user uses to star other repos"
+// @Success 200 {object} controllers.api_controller.Response The response object
+// @router /:user/hitter/update/:hitter [get]
 func (c *UsersController) UpdateUserHitter() {
+	var resp Response
 	user := c.GetString(":user")
 	hitter := c.GetString(":hitter")
 
 	affected := api.UpdateUserHitter(user, hitter)
 
 	if affected {
-		c.Data["json"] = "ok"
+		resp = Response{Code: 200, Msg: "ok", Data: ""}
 	} else {
-		c.Data["json"] = "not affected"
+		resp = Response{Code: 200, Msg: "not affected", Data: ""}
 	}
+
+	c.Data["json"] = resp
 	c.ServeJSON()
 }
