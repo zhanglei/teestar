2178410d692b34868b64f5e15ee53cf0ae4af811
Yang Luo <hsluoyz@qq.com>
2017-08-05 00:14:22 +0800

    Add /api/users/:user/update

--- controllers/api_controllers/users_hitter.go
+++ controllers/api_controllers/users_hitter.go
@@ -7,28 +7,48 @@ import (
 
 // @Title UpdateUserHitter
 // @Description Get the hitter for a user
-// @Param   user     path    string  true        "The username"
-// @Param   hitter     path    string  true        "The GitHub account the user uses to star other repos"
-// @Success 200 {object} controllers.api_controller.Response The response object
-// @router /:user/hitter/update/:hitter [get]
+// @Param   user     path      string  true    "The user that needs to be updated"
+// @Param   hitter   formData  string  true    "The GitHub account the user uses to star other repos"
+// @Param   qq       formData  string  true    "The Tencent QQ account"
+// @Param   nickname formData  string  true    "The Tencent QQ nickname"
+// @Param   email    formData  string  true    "The Email address"
+// @Success 200 {object} controllers.api_controller.Response The Response object
+// @router /:user/update [post]
 func (c *UsersController) UpdateUserHitter() {
 	var resp Response
 	user := c.GetString(":user")
-	hitter := c.GetString(":hitter")
+	hitter := c.Input().Get("hitter")
+	qq := c.Input().Get("qq")
+	nickname := c.Input().Get("nickname")
+	email := c.Input().Get("email")
 
 	msg := api.CheckUserUpdateHitter(user, hitter)
 	if msg != "" {
 		resp = Response{Code: 0, Msg: msg, Data: ""}
-	} else {
-		affected := api.UpdateUserHitter(user, hitter)
-		util.LogInfo(c.Ctx, "API: [%s] updated his hitter", user)
-
-		if affected {
-			resp = Response{Code: 200, Msg: "ok", Data: ""}
-		} else {
-			resp = Response{Code: 200, Msg: "not affected", Data: ""}
-		}
+		c.Data["json"] = resp
+		c.ServeJSON()
+		return
 	}
+
+	api.UpdateUserHitter(user, hitter)
+
+	msg = api.CheckUserUpdateQQ(qq)
+	if msg != "" {
+		resp = Response{Code: 0, Msg: msg, Data: ""}
+		c.Data["json"] = resp
+		c.ServeJSON()
+		return
+	}
+
+	api.UpdateUserQQ(user, qq)
+
+	api.UpdateUserNickname(user, nickname)
+
+	api.UpdateUserEmail(user, email)
+
+	util.LogInfo(c.Ctx, "API: [%s] updated his setting", user)
+
+	resp = Response{Code: 200, Msg: "更新资料成功", Data: ""}
 
 	c.Data["json"] = resp
 	c.ServeJSON()
