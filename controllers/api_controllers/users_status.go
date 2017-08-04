ff5cba9024376a3f51c3af9dfcd72a62dd9d4f86
Yang Luo <hsluoyz@qq.com>
2017-08-04 11:39:55 +0800

    Add docs to Users Status API.

--- controllers/api_controllers/users_status.go
+++ controllers/api_controllers/users_status.go
@@ -2,6 +2,12 @@ package api_controllers
 
 import "github.com/hsluoyz/gitstar/api"
 
+// @Title GetUserTargetStatus
+// @Description Get the status between the user and the target
+// @Param   user     path    string  true        "The username"
+// @Param   target     path    string  true        "Another user"
+// @Success 200 {object} api.UserTargetStatus The Status object
+// @router /:user/status/targets/:target [get]
 func (c *UsersController) GetUserTargetStatus() {
 	user := c.GetString(":user")
 	target := c.GetString(":target")
@@ -10,6 +16,11 @@ func (c *UsersController) GetUserTargetStatus() {
 	c.ServeJSON()
 }
 
+// @Title GetUserStatus
+// @Description Get the status between the user and all other users
+// @Param   user     path    string  true        "The username"
+// @Success 200 {object} []*api.UserTargetStatus The list of Status objects
+// @router /:user/status [get]
 func (c *UsersController) GetUserStatus() {
 	user := c.GetString(":user")
 
@@ -19,6 +30,11 @@ func (c *UsersController) GetUserStatus() {
 	c.ServeJSON()
 }
 
+// @Title GetUserRecommend
+// @Description Get the recommend repos for the user to star
+// @Param   user     path    string  true        "The username"
+// @Success 200 {object} []api.Entry The list of Entry objects
+// @router /:user/status/recommend [get]
 func (c *UsersController) GetUserRecommend() {
 	user := c.GetString(":user")
 
@@ -26,6 +42,11 @@ func (c *UsersController) GetUserRecommend() {
 	c.ServeJSON()
 }
 
+// @Title GetUserOwe
+// @Description Get the details that owes user stars
+// @Param   user     path    string  true        "The username"
+// @Success 200 {object} []*api.UserTargetStatus The list of status objects
+// @router /:user/status/owe [get]
 func (c *UsersController) GetUserOwe() {
 	user := c.GetString(":user")
 
