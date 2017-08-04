336c9bd23dbbe61c4351256d2509e974ea28c2b4
Yang Luo <hsluoyz@qq.com>
2017-08-04 11:14:31 +0800

    Add docs to Users StarringRepo API.

--- controllers/api_controllers/users_starring_repos.go
+++ controllers/api_controllers/users_starring_repos.go
@@ -2,6 +2,11 @@ package api_controllers
 
 import "github.com/hsluoyz/gitstar/api"
 
+// @Title GetUserStarringRepos
+// @Description Get all the repos starred by the user in GitStar cache
+// @Param   user     path    string  true        "The username"
+// @Success 200 {[]string}
+// @router /:user/starring-repos [get]
 func (c *UsersController) GetUserStarringRepos() {
 	user := c.GetString(":user")
 
@@ -9,15 +14,23 @@ func (c *UsersController) GetUserStarringRepos() {
 	c.ServeJSON()
 }
 
+// @Title UpdateUserStarringRepos
+// @Description update all the repos starred by the user into GitStar cache
+// @Param   user     path    string  true        "The username"
+// @Success 200 {object} controllers.api_controller.Response The response object
+// @router /:user/starring-repos/update [get]
 func (c *UsersController) UpdateUserStarringRepos() {
+	var resp Response
 	user := c.GetString(":user")
 
 	affected := api.UpdateUserStarringRepos(user)
 
 	if affected {
-		c.Data["json"] = "ok"
+		resp = Response{Code: 200, Msg: "ok", Data: ""}
 	} else {
-		c.Data["json"] = "not affected"
+		resp = Response{Code: 200, Msg: "not affected", Data: ""}
 	}
+
+	c.Data["json"] = resp
 	c.ServeJSON()
 }
