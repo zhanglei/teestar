bc7207e3075a0b47ed5bdbef55fe2beb0e49d041
Yang Luo <hsluoyz@qq.com>
2017-08-18 00:00:49 +0800

    Show notice to add repo after register.

--- controllers/view_controllers/user.go
+++ controllers/view_controllers/user.go
@@ -78,6 +78,8 @@ func (c *ViewController) Register() {
 	c.SetSessionUser(user)
 
 	util.LogInfo(c.Ctx, "[%s] is registered as new user", user)
+	flash.Success("注册成功！请点击下方“我需要被别人点赞的项目”右侧的“添加项目”，添加你需要被其他人点赞的GitHub项目地址")
+	flash.Store(&c.Controller)
 	c.Redirect("/user/setting", 302)
 }
 
